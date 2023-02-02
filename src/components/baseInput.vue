5b9f7be2c7a280c722641b15669e0edd18e5e460
breligor <77774848+breligor@users.noreply.github.com>
2023-02-02 21:43:09 +0300

    add custom input component

---
+++ src/components/baseInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <input
+    v-model="state.localValue"
+    @keydown.enter="onUpdate"
+    @blur="onUpdate"
+  />
+</template>
+
+<script setup>
+import { reactive } from "vue";
+
+const props = defineProps(["modelValue"]);
+const emit = defineEmits(["update:modelValue"]);
+const state = reactive({
+  localValue: props.modelValue,
+});
+
+const onUpdate = () => {
+  emit("update:modelValue", state.localValue);
+};
+</script>
