6e4e806e7edae95c680498709e03c779c79618c6
breligor <77774848+breligor@users.noreply.github.com>
2023-02-02 21:57:33 +0300

    add form to create task and to add it to firebase database collection

--- src/views/Feed.vue
+++ src/views/Feed.vue
@@ -1,5 +1,47 @@
 <template>
-  <div class="about">
-    <h1>This is FEED</h1>
+  <div>
+    <form @submit.prevent="createNewTask">
+      <label for="newTaskDescription">Task</label>
+      <div>
+        <baseInput
+          v-model="newTask.description"
+          type="text"
+          placeholder="Add task"
+          required
+        />
+      </div>
+      <button type="submit">create</button>
+    </form>
   </div>
 </template>
+
+<script>
+import { reactive, ref } from "vue";
+import { collection, doc, setDoc } from "firebase/firestore";
+import { db } from "@/App.vue";
+
+export default {
+  setup() {
+    const newTask = reactive({
+      creationTime: null,
+      description: "",
+      completed: false,
+    });
+
+    const taskCollection = collection(db, "task");
+    const createNewTask = () => {
+      taskCollection.add({
+        ...newTask,
+        creationTime: Date.now(),
+      });
+      newTask.description = "";
+    };
+    return {
+      newTask,
+      createNewTask,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
