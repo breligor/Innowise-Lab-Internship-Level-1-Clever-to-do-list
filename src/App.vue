db5a76a08fe8fdf441cd8767f6a3478500228d13
breligor <77774848+breligor@users.noreply.github.com>
2023-02-02 21:38:58 +0300

    create App page

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,62 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <!-- <router-link to="/">Home</router-link> | -->
+    <!-- <router-link to="/feed">Feed</router-link> |  -->
+    <!-- <router-link to="/register">Register</router-link>  -->
+    <!-- <router-link to="/sign-in">SignIn</router-link> -->
+    <button @click="handleSignOut" v-if="isLoggedIn">Sign out</button>
   </nav>
   <router-view />
 </template>
 
+<script setup>
+import { onMounted, onBeforeMount, ref } from "vue";
+import { useRouter } from "vue-router";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+
+const router = useRouter();
+const isLoggedIn = ref(false);
+let auth;
+
+onBeforeMount(() => {
+  auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (!user) {
+      router.replace("/sign-in");
+    } else if (router.path == "/register" || router.path == "sign-in") {
+      router.replace("/");
+    }
+  });
+});
+
+onMounted(() => {
+  auth = getAuth();
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      isLoggedIn.value = true;
+    } else {
+      isLoggedIn.value = false;
+    }
+  });
+});
+
+const handleSignOut = () => {
+  signOut(auth).then(() => {
+    router.push("/sign-in");
+    console.log("Succesfully signed out");
+  });
+};
+</script>
+
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
 }
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+main {
+  width: 400px;
+  height: 400px;
 }
 </style>
