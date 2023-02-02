dbc92d3f20ba2ca548ef8dad24c6b529a2d0d11c
breligor <77774848+breligor@users.noreply.github.com>
2023-02-02 21:32:41 +0300

    init root App and firebase auth and database

--- src/main.js
+++ src/main.js
@@ -2,4 +2,22 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-createApp(App).use(router).mount("#app");
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBFSEdbMDCv-GDl-HNgwHtjb2gN8lt2X90",
+  authDomain: "todo-vue-f2bff.firebaseapp.com",
+  databaseURL:
+    "https://todo-vue-f2bff-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "todo-vue-f2bff",
+  storageBucket: "todo-vue-f2bff.appspot.com",
+  messagingSenderId: "915399087042",
+  appId: "1:915399087042:web:ea3945101f2d074ac17c5b",
+};
+const FirebaseApp = firebase.initializeApp(firebaseConfig); //firebase init
+export const db = getFirestore(FirebaseApp); // database
+
+const app = createApp(App);
+app.use(router).mount("#app");
