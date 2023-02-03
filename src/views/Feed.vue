3ee026b5ad05b701b7869812f79a39de7afb02b6
breligor <77774848+breligor@users.noreply.github.com>
2023-02-03 22:19:34 +0300

    add form that create  'to do'-list and set task to firebase database

--- src/views/Feed.vue
+++ src/views/Feed.vue
@@ -1,47 +1,118 @@
 <template>
-  <div>
-    <form @submit.prevent="createNewTask">
-      <label for="newTaskDescription">Task</label>
-      <div>
+  <main>
+    <h1>TO DO LIST</h1>
+    <p>ЧИСЛО ЗАДАЧ:{{ taskArr.length }}</p>
+
+    <div class="formWrapper">
+      <form @submit.prevent="writeUserData" class="form">
         <baseInput
-          v-model="newTask.description"
+          v-model="taskDescription"
           type="text"
-          placeholder="Add task"
+          placeholder="Add task..."
           required
+          class="input"
         />
-      </div>
-      <button type="submit">create</button>
-    </form>
-  </div>
+        <button @click="addTask" type="submit">add task</button>
+      </form>
+    </div>
+    <div>
+      <ol>
+        <li v-for="task in taskArr" :key="task.id">
+          <div>
+            <p :class="{ task_isDone: task.completed }">
+              {{ task.description }}
+            </p>
+          </div>
+          <div>
+            <input v-model="task.completed" type="checkbox" />
+          </div>
+        </li>
+      </ol>
+    </div>
+  </main>
 </template>
 
-<script>
-import { reactive, ref } from "vue";
-import { collection, doc, setDoc } from "firebase/firestore";
-import { db } from "@/App.vue";
+<script setup>
+import { ref, reactive } from "vue";
 
-export default {
-  setup() {
-    const newTask = reactive({
-      creationTime: null,
-      description: "",
-      completed: false,
-    });
+import baseInput from "@/components/baseInput.vue";
+import { db } from "../main.js";
+import { ref as fbRef, set } from "firebase/database";
+import { useRouter } from "vue-router";
+import { getAuth } from "firebase/auth";
 
-    const taskCollection = collection(db, "task");
-    const createNewTask = () => {
-      taskCollection.add({
-        ...newTask,
-        creationTime: Date.now(),
-      });
-      newTask.description = "";
-    };
-    return {
-      newTask,
-      createNewTask,
-    };
-  },
+const router = useRouter();
+const taskDescription = ref("");
+const auth = getAuth();
+const user = auth.currentUser;
+const userId = user.uid;
+const currentDate = Date.now();
+const taskArr = reactive([]);
+const isComplete = false;
+const date = new Date();
+const taskId = date.setDate(date.getDate());
+
+//set data to firebase db
+function writeUserData() {
+  set(
+    fbRef(
+      db,
+      "users/" +
+        `${"userId : " + userId}/${"currentDate : " + currentDate}/${
+          "taskId : " + taskId
+        }`
+    ),
+    {
+      mail: user.email,
+      taskDescription: taskDescription.value,
+      completed: isComplete,
+      creationTime: currentDate,
+    }
+  );
+}
+
+//add task to task list
+const addTask = () => {
+  const newTask = {
+    description: taskDescription.value,
+    id: taskId,
+    completed: isComplete,
+  };
+  taskArr.push(newTask);
+  //taskDescription.value = ""; clear input after submit
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+main {
+  display: flex;
+  justify-content: start;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+}
+button {
+  width: 100px;
+}
+.formWrapper {
+  width: 800px;
+  display: flex;
+  justify-content: center;
+}
+.form {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+.input {
+  width: 100%;
+  max-width: 500px;
+  height: 30px;
+}
+.task_isDone {
+  text-decoration: line-through;
+}
+button {
+  cursor: pointer;
+}
+</style>
