37b996e10f451617d935bad47d7848ae46e4c1cd
breligor <77774848+breligor@users.noreply.github.com>
2023-02-07 22:48:08 +0300

    change get function to function for realtime update from firestore

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,53 @@
 <template>
   <div class="wrapperTodo">
     <div class="title has-text-centered">WHAT's TODO</div>
-    <div class="field is-grouped mb-5">
-      <p class="control is-expanded">
-        <input class="input" type="text" placeholder="Add a todo..." />
-      </p>
-      <p class="control">
-        <a class="button is-info"> Add </a>
-      </p>
-    </div>
 
-    <div class="card mb-5"
-    v-for = "i in 3"
+    <form @submit.prevent="addTodo">
+      <div class="field is-grouped mb-5">
+        <p class="control is-expanded">
+          <input
+            v-model="newTodoContent"
+            class="input"
+            type="text"
+            placeholder="Add a todo..."
+          />
+        </p>
+        <p class="control">
+          <button :disabled="!newTodoContent" class="button is-info">
+            Add
+          </button>
+        </p>
+      </div>
+    </form>
 
+    <div
+      :class="{ 'has-background-success-light': todo.done }"
+      class="card mb-5"
+      v-for="todo in todos"
     >
       <div class="card-content">
         <div class="content">
           <div class="columns is-mobile is-vcentered">
-            <div class="column">my btn</div>
+            <div
+              class="column"
+              :class="{ 'has-text-success line-through': todo.done }"
+            >
+              {{ todo.content }}
+            </div>
             <div class="column is-3 had-text-right">
-              <button class="button is-light">&check;</button>
-              <button class="button is-danger ml-2">&cross;</button>
+              <button
+                @click="toggleDone(todo.id)"
+                :class="todo.done ? 'is-success' : 'is-light'"
+                class="button"
+              >
+                &check;
+              </button>
+              <button
+                @click="deleteTodo(todo.id)"
+                class="button is-danger ml-2"
+              >
+                &cross;
+              </button>
             </div>
           </div>
         </div>
@@ -29,7 +56,70 @@
   </div>
 </template>
 
-<script></script>
+<script setup>
+import { ref, onMounted } from "vue";
+import { v4 as uuidv4 } from "uuid";
+import { dbStore } from "@/main";
+import { collection, onSnapshot } from "@firebase/firestore";
+
+const todos = ref([
+ 
+]);
+
+const newTodoContent = ref("");
+
+const addTodo = () => {
+  const newTodo = {
+    id: uuidv4(),
+    content: newTodoContent.value,
+    done: false,
+  };
+  todos.value.unshift(newTodo);
+  newTodoContent.value = "";
+};
+
+//get todos
+
+onMounted(async () => {
+  //   const querySnapshot = await getDocs(collection(dbStore, "todos"));
+  //   let fbTodos = []
+  // querySnapshot.forEach((doc) => {
+  //    console.log(doc.id, " => ", doc.data());
+  //    const todo = {
+  //     id: doc.id,
+  //     content: doc.data().content,
+  //     done: doc.data().done
+  //    }
+  //    fbTodos.push(todo)
+  // });
+  // 
+
+  //update
+  onSnapshot(collection(dbStore, "todos"), (querySnapshot) => {
+    const fbTodos = [];
+    querySnapshot.forEach((doc) => {
+      const todo = {
+        id: doc.id,
+        content: doc.data().content,
+        done: doc.data().done,
+      };
+      fbTodos.push(todo);
+    });
+    todos.value = fbTodos
+  });
+});
+
+//del btn
+const deleteTodo = (id) => {
+  todos.value = todos.value.filter((todo) => todo.id !== id);
+};
+
+
+const toggleDone = (id) => {
+  const index = todos.value.findIndex((todo) => todo.id === id);
+  todos.value[index].done = !todos.value[index].done;
+};
+</script>
 
 <style>
 @import "bulma/css/bulma.min.css";
@@ -40,5 +130,8 @@
   display: flex;
   justify-content: center;
   flex-direction: column;
+}
+.line-through {
+  text-decoration: line-through;
 }
 </style>
