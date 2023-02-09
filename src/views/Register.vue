e6a222f1e509d4274588b892701f1e1d1dd1f441
breligor <77774848+breligor@users.noreply.github.com>
2023-02-09 09:11:05 +0300

    change UI styles for signup form with UI library

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,34 +1,79 @@
 <template>
   <main>
-    
-    <h1>Create your own account!</h1>
-    <div>
-      <baseInput class="input" v-model="email" type="email" placeholder="Email" />
+    <div class="box">
+      <div class="block is-flex is-justify-content-center">
+        <h1 class="subtitle">create your todo</h1>
+      </div>
+
+      <div class="tabs">
+        <ul class="is-justify-content-center">
+          <li>
+            <router-link class="link" to="/sign-in">Sign in</router-link>
+          </li>
+          <li class="is-active"><a>Register</a></li>
+        </ul>
+      </div>
+      <div class="field">
+        <p class="control has-icons-left has-icons-right">
+          <baseInput
+            class="input"
+            v-model="email"
+            type="email"
+            placeholder="Email"
+          />
+          <span class="icon is-small is-left">
+            <i class="fas fa-envelope"></i>
+          </span>
+          <span class="icon is-small is-right">
+            <i class="fas fa-check"></i>
+          </span>
+        </p>
+      </div>
+      <div class="field">
+        <p class="control has-icons-left">
+          <baseInput
+            class="input"
+            v-model="password"
+            type="password"
+            placeholder="Password"
+          />
+          <span class="icon is-small is-left">
+            <i class="fas fa-lock"></i>
+          </span>
+        </p>
+        <div class="block is-flex is-justify-content-center">
+          <p class="subtitle" v-if="errMsg">{{ errMsg }}</p>
+        </div>
+      </div>
+      <div class="field">
+        <p class="control has-icons-left">
+          <baseInput
+            class="input"
+            v-model="confirmPassword"
+            type="password"
+            placeholder="Confirm password"
+          />
+          <span class="icon is-small is-left">
+            <i class="fas fa-lock"></i>
+          </span>
+        </p>
+        <div class="block is-flex is-justify-content-center">
+          <p class="subtitle" v-if="errMsg">{{ errMsg }}</p>
+        </div>
+      </div>
+      <div class="field">
+        <p class="control is-flex is-justify-content-center">
+          <button @click="register" class="button is-success">Login</button>
+        </p>
+      </div>
     </div>
-    <div>
-      <baseInput class="input" v-model="password" type="password" placeholder="Password" />
-    </div>
-    <div>
-      <baseInput class="input"
-        v-model="confirmPassword"
-        type="password"
-        placeholder="Confirm password"
-      />
-    </div>
-    <div>
-      <baseInput class="input" v-model="userName" type="text" placeholder="Name" />
-    </div>
-    <button class="btn" @click="register">Sign up</button>
-    <nav>
-      <router-link class="link" to="/sign-in">SIGN IN</router-link>
-    </nav>
   </main>
 </template>
 
 <script setup>
 import { ref } from "vue";
 
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 import { useRouter } from "vue-router";
 import baseInput from "@/components/baseInput.vue";
 
@@ -37,7 +82,7 @@ const password = ref("");
 const userName = ref("");
 const confirmPassword = ref("");
 const router = useRouter();
-
+const auth = getAuth();
 const register = () => {
   if (confirmPassword.value === password.value) {
     createUserWithEmailAndPassword(auth, email.value, password.value)
@@ -58,43 +103,44 @@ const register = () => {
 
 <style lang="scss" scoped>
 main {
-  background-color: rgb(100, 50, 122);
   display: flex;
-  justify-content: center;
+  justify-content: start;
   align-items: center;
   flex-direction: column;
-  box-shadow: 0 10px 10px -5px black;
-  border-radius: 3px;
+  height: 100vh;
+  width: 100%;
+  padding-top: 50px;
 }
-.input {
-  margin-bottom: 10px;
-  width: 200px;
-  height: 30px;
-  border: 0;
-  border-bottom: 2px solid rgb(255, 251, 251);
-  outline: 0;
-  color: antiquewhite;
-  padding: 7px 0;
-  background: transparent;
-  transition: border-color 0.2s;
-}
-h1 {
-  color: antiquewhite;
-  font-size: 15px;
-  letter-spacing: 2px;
-}
-.btn {
-  margin-bottom: 15px;
-  margin-top: 10px;
-  width: 80px;
-  height: 30px;
-  &:hover {
-    cursor: pointer;
-  }
-}
-.link {
-  text-decoration: none;
-  color: antiquewhite;
-  font-size: 15px;
-}
+
+// .input {
+//   margin-bottom: 10px;
+//   width: 200px;
+//   height: 30px;
+//   border: 0;
+//   border-bottom: 2px solid rgb(255, 251, 251);
+//   outline: 0;
+//   color: antiquewhite;
+//   padding: 7px 0;
+//   background: transparent;
+//   transition: border-color 0.2s;
+// }
+// h1 {
+//   color: antiquewhite;
+//   font-size: 15px;
+//   letter-spacing: 2px;
+// }
+// .btn {
+//   margin-bottom: 15px;
+//   margin-top: 10px;
+//   width: 80px;
+//   height: 30px;
+//   &:hover {
+//     cursor: pointer;
+//   }
+// }
+// .link {
+//   text-decoration: none;
+//   color: antiquewhite;
+//   font-size: 15px;
+// }
 </style>
