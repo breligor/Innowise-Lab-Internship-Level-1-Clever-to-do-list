586fd3c64ba8134eaeeb04253b0dbefb3ea56251
breligor <77774848+breligor@users.noreply.github.com>
2023-02-09 09:08:00 +0300

    add edit function for task and new UI styles

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,64 +1,85 @@
 <template>
-  <div class="wrapperTodo">
-    <div class="title has-text-centered">WHAT's TODO</div>
-
-    <form @submit.prevent="addTodo">
-      <div class="field is-grouped mb-5">
-        <p class="control is-expanded">
-          <input
-            v-model="newTodoContent"
-            class="input"
-            type="text"
-            placeholder="Add a todo..."
-          />
-        </p>
-        <p class="control">
-          <button :disabled="!newTodoContent" class="button is-info">
-            Add
-          </button>
-        </p>
+  <main>
+    <div class="wrapperTodo">
+      <div class="btnWrapper">
+        <button @click="handleSignOut" v-if="isLoggedIn">Sign out</button>
       </div>
-    </form>
+      <div class="title has-text-centered">WHAT's TODO</div>
 
-    <div
-      :class="{ 'has-background-success-light': todo.done }"
-      class="card mb-5"
-      v-for="todo in todos"
-    >
-      <div class="card-content">
-        <div class="content">
-          <div class="columns is-mobile is-vcentered">
-            <div
-              class="column"
-              :class="{ 'has-text-success line-through': todo.done }"
-            >
-              {{ todo.content }}
-            </div>
-            <div class="column is-3 had-text-right">
-              <button
-                @click="toggleDone(todo.id)"
-                :class="todo.done ? 'is-success' : 'is-light'"
-                class="button"
-              >
-                &check;
-              </button>
-              <button
-                @click="deleteTodo(todo.id)"
-                class="button is-danger ml-2"
-              >
-                &cross;
-              </button>
+      <form @submit.prevent="addTodo">
+        <div class="field is-grouped mb-5">
+          <p class="control is-expanded">
+            <input
+              v-model="newTodoContent"
+              class="input"
+              type="text"
+              placeholder="Add a todo..."
+            />
+          </p>
+          <p class="control">
+            <button :disabled="!newTodoContent" class="button is-info">
+              Add
+            </button>
+          </p>
+        </div>
+      </form>
+      <div class="todo-item-wrapper">
+        <div
+          :class="{ 'has-background-success-light': todo.done }"
+          class="card mb-5"
+          v-for="todo in todos"
+        >
+          <div class="card-content">
+            <div class="content">
+              <div class="is-flex is-justify-content-space-between">
+                <div class="is-flex">
+                  <div
+                    v-if="!todo.editing"
+                    @dblclick="editTodo(todo)"
+                    class="column"
+                    :class="{ 'has-text-success line-through': todo.done }"
+                    title="double click to edit"
+                  >
+                    {{ todo.content }}
+                  </div>
+                  <input
+                    v-else
+                    class="input"
+                    v-model="todo.content"
+                    type="text"
+                    @blur="doneEdit(todo, todo.id)"
+                    @keyup.enter="doneEdit(todo, todo.id)"
+                  />
+                </div>
+
+                <div class="column is-3 had-text-right">
+                  <button
+                    @click="toggleDone(todo.id)"
+                    :class="todo.done ? 'is-success' : 'is-light'"
+                    class="button"
+                  >
+                    &check;
+                  </button>
+                  <button
+                    @click="deleteTodo(todo.id)"
+                    class="button is-danger ml-2"
+                  >
+                    &cross;
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
+  </main>
 </template>
 
 <script setup>
 import { ref, onMounted } from "vue";
 import { dbStore } from "@/main";
+import { getAuth } from "firebase/auth";
 import {
   doc,
   collection,
@@ -66,16 +87,27 @@ import {
   addDoc,
   deleteDoc,
   updateDoc,
+  query,
+  orderBy,
+  limit,
 } from "@firebase/firestore";
 
+const auth = getAuth();
+const user = auth.currentUser;
+const userId = user.uid;
 const todos = ref([]);
-const todosCollectionRef = collection(dbStore, "todos");
 const newTodoContent = ref("");
+const todosCollectionRef = collection(dbStore, `${userId}`);
+const todosCollectionQuery = query(todosCollectionRef, orderBy("date", "desc"));
 
 const addTodo = () => {
   addDoc(todosCollectionRef, {
     content: newTodoContent.value,
     done: false,
+    date: Date.now(),
+    userId: userId,
+    mail: user.email,
+    editing: false,
   });
   newTodoContent.value = "";
 };
@@ -98,13 +130,16 @@ const addTodo = () => {
 
 //update todos
 onMounted(() => {
-  onSnapshot(todosCollectionRef, (querySnapshot) => {
+  onSnapshot(todosCollectionQuery, (querySnapshot) => {
     const fbTodos = [];
     querySnapshot.forEach((doc) => {
       const todo = {
         id: doc.id,
         content: doc.data().content,
         done: doc.data().done,
+        userId: userId,
+        mail: user.email,
+        editing: false,
       };
       fbTodos.push(todo);
     });
@@ -127,10 +162,32 @@ const toggleDone = (id) => {
     done: !todos.value[index].done,
   });
 };
+
+//editing
+const editTodo = (todo) => {
+  todo.editing = true;
+};
+
+const doneEdit = (todo, id) => {
+  todo.editing = false;
+  const index = todos.value.findIndex((todo) => todo.id === id);
+  updateDoc(doc(todosCollectionRef, id), {
+    content: todos.value[index].content,
+  });
+};
 </script>
 
 <style>
 @import "bulma/css/bulma.min.css";
+main {
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+  width: 100%;
+  padding-top: 50px;
+}
 .wrapperTodo {
   width: 100%;
   max-width: 600px;
@@ -141,5 +198,9 @@ const toggleDone = (id) => {
 }
 .line-through {
   text-decoration: line-through;
+}
+.todo-item-wrapper {
+  min-height: 70%vh;
+  overflow: hidden;
 }
 </style>
