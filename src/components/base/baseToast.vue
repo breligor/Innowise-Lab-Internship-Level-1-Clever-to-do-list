0d524f81527d75764574944a850269c7730859f2
Igor Brel <>
2023-02-21 14:05:24 +0300

    uninstall toast library, add custom toast component

---
+++ src/components/base/baseToast.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="notification is-danger is-light">
+    <button class="delete" @click="closeToast()"></button>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "baseToast",
+  props: ["errMessage"],
+  methods: {
+    closeToast() {
+      this.$emit("closeToast", this.event);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "bulma/css/bulma.min.css";
+</style>
