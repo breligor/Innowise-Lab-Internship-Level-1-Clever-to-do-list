761eaa9f231a1dd1acac1db15203e10ffb2f2858
breligor <77774848+breligor@users.noreply.github.com>
2023-02-21 20:34:35 +0300

    refactor composable useNotificationApi

--- src/toastFunctions.js
+++ src/toastFunctions.js
@@ -1,34 +1,14 @@
-import { useToast } from "vue-toastification";
-
-const toast = useToast();
+import { ref } from "vue";
 
 export function useNotificationApi() {
+  const errMessage = ref("");
 
-  const options = {
-    position: "top-center",
-    timeout: 2970,
-    closeOnClick: true,
-    pauseOnFocusLoss: false,
-    pauseOnHover: false,
-    draggable: true,
-    draggablePercent: 0.6,
-    showCloseButtonOnHover: false,
-    hideProgressBar: true,
-    closeButton: "button",
-    icon: true,
-    rtl: false,
-    transition: "Vue-Toastification__bounce",
-    maxToasts: 20,
-    newestOnTop: true,
+  const closeToast = () => {
+    errMessage.value = "";
   };
 
-  const showToastError = (data) => {
-    toast.error(data);
+  const autoHideToast = () => {
+    setTimeout(() => (errMessage.value = ""), 3000);
   };
-  
-  const showToastSuccess = (data) => {
-    toast.success(data);
-  };
-
-  return { options, showToastError, showToastSuccess };
+  return { errMessage, closeToast, autoHideToast };
 }
